6a8cbb203cc7874ba3601f064038ba2bf4fb7cf7
holjas <holly.jasiura@gmail.com>
2021-07-26 17:38:26 -0400

    added search bar

--- main.js
+++ main.js
@@ -5,12 +5,15 @@ const app = Vue.createApp({
     };
   },
   created() {
-    fetch("./response.json")
-      .then((res) => res.json())
-      //   .then((res) => console.log(res.results))
+    const url = new URL("https://api.unsplash.com/search/photos/");
+    url.search = new URLSearchParams({
+      client_id: "OuD9fBBhhqUwYF8d6yVIT8yFdIR3AQk9MtF_bC7Fjws",
+      orientation: "squarish",
+      query: "painting",
+    });
+    fetch(url)
+      .then((response) => response.json())
       .then((data) => (this.details = data.results));
-
-    console.log(this.details);
   },
 
   methods: {},

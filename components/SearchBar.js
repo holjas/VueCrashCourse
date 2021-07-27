5d1b3411cf5da1c71eb4873c1e5c339928e96b19
holjas <holly.jasiura@gmail.com>
2021-07-27 15:43:10 -0400

    :bug:

---
+++ components/SearchBar.js
@@ -0,0 +1,31 @@
+app.component("search-bar", {
+  data() {
+    return { searchTerm: "" };
+  },
+  template:
+    /*html*/
+    `
+    <nav class="navbar navbar-light bg-light">
+    <div class="container-fluid">
+      <form class="d-flex" @submit.prevent="onSubmit">
+        <input
+          class="form-control me-2"
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+          v-model="searchTerm"
+        />
+        <button class="btn btn-outline-primary" type="submit" >
+          Search
+        </button>
+      </form>
+    </div>
+  </nav>
+    `,
+  methods: {
+    onSubmit() {
+      console.log("let's emit");
+      this.$emit("", this.searchTerm);
+    },
+  },
+});
--- main.js
+++ main.js
@@ -5,22 +5,14 @@ const app = Vue.createApp({
       searchTerm: "",
     };
   },
+  //   mounted() {
+  //     this.fetchCatalogue("city");
+  //   },
+  //   LOCAL DATA -------------------------
   mounted() {
-    this.fetchCatalogue("city");
-    //   https://unsplash.com/documentation#search-photos //API DOCS
-    // const url = new URL("https://api.unsplash.com/search/photos/");
-    // url.search = new URLSearchParams({
-    //   client_id: "OuD9fBBhhqUwYF8d6yVIT8yFdIR3AQk9MtF_bC7Fjws",
-    //   orientation: "squarish",
-    //   per_page: 50,
-    //   query: this.searchTerm,
-    // });
-    // fetch(url)
-    //   .then((response) => response.json())
-    //   .then((data) => (this.details = data.results));
-    // fetch("./response.json")
-    //   .then((res) => res.json())
-    //   .then((data) => (this.details = data.results));
+    fetch("./response.json")
+      .then((res) => res.json())
+      .then((data) => (this.details = data.results));
   },
   methods: {
     fetchCatalogue(imageSearch) {

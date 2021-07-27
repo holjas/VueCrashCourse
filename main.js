bb981448af223a9b6dd2e0c6d43b82bb13f8da0f
holjas <holly.jasiura@gmail.com>
2021-07-27 15:19:39 -0400

    default search item set so screen wont be blank

--- main.js
+++ main.js
@@ -2,19 +2,41 @@ const app = Vue.createApp({
   data() {
     return {
       details: [],
+      searchTerm: "",
     };
   },
-  created() {
-    const url = new URL("https://api.unsplash.com/search/photos/");
-    url.search = new URLSearchParams({
-      client_id: "OuD9fBBhhqUwYF8d6yVIT8yFdIR3AQk9MtF_bC7Fjws",
-      orientation: "squarish",
-      query: "painting",
-    });
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => (this.details = data.results));
+  mounted() {
+    this.fetchCatalogue("city");
+    //   https://unsplash.com/documentation#search-photos //API DOCS
+    // const url = new URL("https://api.unsplash.com/search/photos/");
+    // url.search = new URLSearchParams({
+    //   client_id: "OuD9fBBhhqUwYF8d6yVIT8yFdIR3AQk9MtF_bC7Fjws",
+    //   orientation: "squarish",
+    //   per_page: 50,
+    //   query: this.searchTerm,
+    // });
+    // fetch(url)
+    //   .then((response) => response.json())
+    //   .then((data) => (this.details = data.results));
+    // fetch("./response.json")
+    //   .then((res) => res.json())
+    //   .then((data) => (this.details = data.results));
   },
-
-  methods: {},
+  methods: {
+    fetchCatalogue(imageSearch) {
+      const url = new URL("https://api.unsplash.com/search/photos/");
+      url.search = new URLSearchParams({
+        client_id: "OuD9fBBhhqUwYF8d6yVIT8yFdIR3AQk9MtF_bC7Fjws",
+        orientation: "squarish",
+        per_page: 20,
+        query: imageSearch,
+      });
+      fetch(url)
+        .then((response) => response.json())
+        .then((data) => (this.details = data.results));
+    },
+    onSubmit() {
+      this.fetchCatalogue(this.searchTerm);
+    },
+  },
 });
